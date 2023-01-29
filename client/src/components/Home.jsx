53cbd14343605d829c5bf1bc0a7bfeab00f29b87
madahva <galarzaguillermo.ggm@gmail.com>
2023-01-29 12:56:36 -0300

    Style Nav

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPokemons, getAllTypes } from "../redux/actions.js";
 
+import css from "../assets/styles/Home.module.css";
 import FilterBar from "./FilterBar.jsx"
 import Cards from "./Cards.jsx";
 import Pagination from "./Pagination.jsx";
@@ -17,7 +18,7 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <div>
+    <div className={css.home}>
       <FilterBar pokemons={pokemons} />
       <Cards pokemons={pokemons} />
       <Pagination pokemons={pokemons} />
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,13 +1,19 @@
-import { Link  } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import css from "../assets/styles/Nav.module.css";
 const Nav = () => {
   return (
-    <div>
-      <Link to="/">Exit</Link>
-      <Link to="/home">Home</Link>
-      <Link to="/create">Create</Link>
+    <div className={css.nav}>
+      <Link to="/">
+        <button tabIndex={-1}>Exit</button>
+      </Link>
+      <Link to="/home">
+        <button tabIndex={-1}>Home</button>
+      </Link>
+      <Link to="/create">
+        <button tabIndex={-1}>Create</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
