94474ac3d6b5c8bfe0f1d2b3952540482771293a
madahva <galarzaguillermo.ggm@gmail.com>
2023-04-02 22:51:49 -0300

    test

--- api/src/db.js
+++ api/src/db.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
-/* 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
   {
@@ -12,12 +11,7 @@ const sequelize = new Sequelize(
     native: false,
   }
 );
-*/
 
-const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false,
-  native: false,
-});
 
 const basename = path.basename(__filename);
 const modelDefiners = [];
